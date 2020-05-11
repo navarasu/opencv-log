9a252e79e1085392c4929757a1e7a68ca94f89f4
Gasol Wu <gasol.wu@gmail.com>
2020-05-11 16:15:56 +0800

    Log hough_circles

--- tests/test_log.py
+++ tests/test_log.py
@@ -89,6 +89,18 @@ def test_log_hough_lines():
     logitem = get_html('log/cvlog.html').select('.log-list .log-item')
     assert logitem[0]['logdata'] == read_file('tests/data/expected/houghline_img.txt')
 
+def test_log_hough_circles():
+    remove_dirs('log/')
+    img = cv2.imread('tests/data/board.jpg')
+    log.set_mode(log.Mode.LOG)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.medianBlur(gray, 5)
+    circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, 10, np.array([]), 100, 30, 1, 30)
+
+    log.hough_circles(log.Level.ERROR, circles, img)
+    logitem = get_html('log/cvlog.html').select('.log-list .log-item')
+    assert logitem[0]['logdata'] == read_file('tests/data/expected/houghcircle_img.txt')
+
 def log_all_level(img):
     log.image(log.Level.TRACE, img)
     log.image(log.Level.INFO, img)
